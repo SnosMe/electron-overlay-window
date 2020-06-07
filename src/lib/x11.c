e0088a30e218655e1bcc914059821ad509ec9da2
Alexander Drozdov <snosme@gmail.com>
2020-06-07 17:44:28 +0300

    almost done

--- src/lib/x11.c
+++ src/lib/x11.c
@@ -7,41 +7,135 @@
 
 static xcb_connection_t* x_conn;
 static xcb_window_t root;
-static xcb_window_t active_window = XCB_WINDOW_NONE;
 static xcb_atom_t ATOM_NET_ACTIVE_WINDOW;
 static xcb_atom_t ATOM_NET_WM_NAME;
 static xcb_atom_t ATOM_UTF8_STRING;
+static xcb_atom_t ATOM_NET_WM_STATE;
+static xcb_atom_t ATOM_NET_WM_STATE_FULLSCREEN;
 
 struct ow_target_window
 {
   char* title;
   xcb_window_t window_id;
   bool is_focused;
+  bool is_destroyed;
+  bool is_fullscreen;
 };
+
+struct ow_overlay_window
+{
+  xcb_window_t window_id;
+};
+
+static xcb_window_t active_window = XCB_WINDOW_NONE;
 
 static struct ow_target_window target_info = {
   .title = NULL,
   .window_id = XCB_WINDOW_NONE,
-  .is_focused = false
+  .is_focused = false,
+  .is_destroyed = false,
+  .is_fullscreen = false // initial state of *overlay* window
 };
 
+static struct ow_overlay_window overlay_info = {
+  .window_id = XCB_WINDOW_NONE
+};
+
+static xcb_window_t get_active_window() {
+  xcb_get_property_reply_t* prop_reply = xcb_get_property_reply(x_conn, xcb_get_property(x_conn, 0, root, ATOM_NET_ACTIVE_WINDOW, XCB_ATOM_WINDOW, 0, 1), NULL);
+  if (prop_reply == NULL) {
+    return XCB_WINDOW_NONE;
+  }
+  xcb_window_t active_window = *((xcb_window_t*)xcb_get_property_value(prop_reply));
+  free(prop_reply);
+  return active_window;
+}
+
 static bool get_title(xcb_window_t wid, char** title) {
-  xcb_get_property_reply_t* active_win_reply = xcb_get_property_reply(x_conn, xcb_get_property(x_conn, 0, wid, ATOM_NET_WM_NAME, ATOM_UTF8_STRING, 0, 100000), NULL);
-  if (active_win_reply == NULL) {
+  if (wid == XCB_WINDOW_NONE) {
+    *title = NULL;
+    return true;
+  }
+  xcb_get_property_reply_t* prop_reply = xcb_get_property_reply(x_conn, xcb_get_property(x_conn, 0, wid, ATOM_NET_WM_NAME, ATOM_UTF8_STRING, 0, 100000), NULL);
+  if (prop_reply == NULL) {
     return false;
   }
-  int buffLenUtf8 = xcb_get_property_value_length(active_win_reply);
+  int buffLenUtf8 = xcb_get_property_value_length(prop_reply);
   if (buffLenUtf8 == 0) {
-    // @TODO: fallback to WM_NAME ?
     *title = NULL;
-    free(active_win_reply);
+    free(prop_reply);
     return true;
   }
   *title = malloc(buffLenUtf8 + 1);
-  memcpy(*title, xcb_get_property_value(active_win_reply), buffLenUtf8);
+  memcpy(*title, xcb_get_property_value(prop_reply), buffLenUtf8);
   (*title)[buffLenUtf8] = '\0';
-  free(active_win_reply);
+  free(prop_reply);
   return true;
+}
+
+static bool get_content_bounds(xcb_window_t wid, struct ow_window_bounds* bounds) {
+  xcb_get_geometry_reply_t* geometry = xcb_get_geometry_reply(x_conn, xcb_get_geometry(x_conn, wid), NULL);
+  if (geometry == NULL) {
+    return false;
+  }
+  xcb_translate_coordinates_reply_t* translated = xcb_translate_coordinates_reply(x_conn, xcb_translate_coordinates(x_conn, wid, root, 0, 0), NULL);
+  if (translated == NULL) {
+    free(geometry);
+    return false;
+  }
+
+  bounds->x = translated->dst_x;
+  bounds->y = translated->dst_y;
+  bounds->width = geometry->width;
+  bounds->height = geometry->height;
+  free(translated);
+  free(geometry);
+  return true;
+}
+
+static bool is_fullscreen_window(xcb_window_t wid, bool* is_fullscreen) {
+  xcb_get_property_reply_t* prop_reply = xcb_get_property_reply(x_conn, xcb_get_property(x_conn, 0, wid, ATOM_NET_WM_STATE, XCB_ATOM_ATOM, 0, 100000), NULL);
+  if (prop_reply == NULL) {
+    return false;
+  }
+  *is_fullscreen = false;
+  xcb_atom_t* wm_state = (xcb_atom_t*)xcb_get_property_value(prop_reply);
+  for (unsigned i = 0; i < prop_reply->value_len; ++i) {
+    if (wm_state[i] == ATOM_NET_WM_STATE_FULLSCREEN) {
+      *is_fullscreen = true;
+    }
+  }
+  free(prop_reply);
+  return true;
+}
+
+static void handle_moveresize_xevent(struct ow_target_window* target_info) {
+  struct ow_window_bounds bounds;
+  if (get_content_bounds(target_info->window_id, &bounds)) {
+    struct ow_event e = {
+      .type = OW_MOVERESIZE,
+      .data.moveresize = {
+        .bounds = bounds
+      }
+    };
+    ow_emit_event(&e);
+  }
+}
+
+static void handle_fullscreen_xevent(struct ow_target_window* target_info) {
+  bool is_fullscreen;
+  if (is_fullscreen_window(target_info->window_id, &is_fullscreen)) {
+    if (is_fullscreen != target_info->is_fullscreen) {
+      target_info->is_fullscreen = is_fullscreen;
+      struct ow_event e = {
+        .type = OW_FULLSCREEN,
+        .data.fullscreen = {
+          .is_fullscreen = target_info->is_fullscreen
+        }
+      };
+      ow_emit_event(&e);
+    }
+  }
 }
 
 static void check_and_handle_window(xcb_window_t wid, struct ow_target_window* target_info) {
@@ -53,12 +147,9 @@ static void check_and_handle_window(xcb_window_t wid, struct ow_target_window* t
         ow_emit_event(&e);
       }
 
-      // @TODO: test if closed
-      bool targetIsClosed = false;
-      if (targetIsClosed) {
-        // @TODO set _NET_WM_STATE_HIDDEN
-        // xcb_flush(x_conn);
+      if (target_info->is_destroyed) {
         target_info->window_id = XCB_WINDOW_NONE;
+        target_info->is_destroyed = false;
         struct ow_event e = { .type = OW_DETACH };
         ow_emit_event(&e);
       }
@@ -75,10 +166,8 @@ static void check_and_handle_window(xcb_window_t wid, struct ow_target_window* t
 
   char* title = NULL;
   if (!get_title(wid, &title) || title == NULL) {
-    printf("untitled=%x\n", wid);
     return;
   }
-  printf("title=\"%s\"\n", title);
   bool is_equal = (strcmp(title, target_info->title) == 0);
   free(title);
   if (!is_equal) {
@@ -89,32 +178,68 @@ static void check_and_handle_window(xcb_window_t wid, struct ow_target_window* t
     uint32_t mask[] = { XCB_EVENT_MASK_NO_EVENT };
     xcb_change_window_attributes(x_conn, target_info->window_id, XCB_CW_EVENT_MASK, mask);
   }
-  target_info->window_id = wid;
 
-  // listen for `_NET_WM_STATE` fullscreen and window move/resize
+  target_info->window_id = wid;
+  xcb_change_property(x_conn, XCB_PROP_MODE_REPLACE, overlay_info.window_id, XCB_ATOM_WM_TRANSIENT_FOR, XCB_ATOM_WINDOW, 32, 1, (void*)&target_info->window_id);
+
+  // listen for `_NET_WM_STATE` fullscreen and window move/resize/destroy
   uint32_t mask[] = { XCB_EVENT_MASK_PROPERTY_CHANGE | XCB_EVENT_MASK_STRUCTURE_NOTIFY };
   xcb_change_window_attributes(x_conn, target_info->window_id, XCB_CW_EVENT_MASK, mask);
+  xcb_flush(x_conn);
+
+  struct ow_event e = {
+    .type = OW_ATTACH,
+    .data.attach = {
+      .has_access = -1
+    }
+  };
+  ow_emit_event(&e);
+  target_info->is_focused = true;
+  e.type = OW_FOCUS;
+  ow_emit_event(&e);
 }
 
-static xcb_window_t get_active_window() {
-  xcb_get_property_reply_t* active_win_reply = xcb_get_property_reply(x_conn, xcb_get_property(x_conn, 0, root, ATOM_NET_ACTIVE_WINDOW, XCB_ATOM_WINDOW, 0, 1), NULL);
-  if (active_win_reply == NULL) {
-    return XCB_WINDOW_NONE;
-  }
-  xcb_window_t active_window = *((xcb_window_t*)xcb_get_property_value(active_win_reply));
-  free(active_win_reply);
-  return active_window;
-}
-
-static void hook_proc(xcb_generic_event_t* event) {
-  if (event->response_type & XCB_PROPERTY_NOTIFY) {
-    xcb_property_notify_event_t* prop = (xcb_property_notify_event_t*)event;
-    if (prop->window == root && prop->atom == ATOM_NET_ACTIVE_WINDOW) {
-      check_and_handle_window(get_active_window(), &target_info);
+static void hook_proc(xcb_generic_event_t* generic_event) {
+  if (generic_event->response_type == XCB_DESTROY_NOTIFY) {
+    xcb_destroy_notify_event_t* event = (xcb_destroy_notify_event_t*)generic_event;
+    if (event->window == target_info.window_id) {
+      target_info.is_destroyed = true;
+      check_and_handle_window(XCB_WINDOW_NONE, &target_info);
     }
     return;
   }
-  printf("[0] Unhanled Event\n");
+  if (generic_event->response_type == XCB_CONFIGURE_NOTIFY) {
+    xcb_configure_notify_event_t* event = (xcb_configure_notify_event_t*)generic_event;
+    if (event->window == target_info.window_id) {
+      handle_moveresize_xevent(&target_info);
+    }
+    return;
+  }
+  if (generic_event->response_type == XCB_PROPERTY_NOTIFY) {
+    xcb_property_notify_event_t* event = (xcb_property_notify_event_t*)generic_event;
+    if (event->window == root && event->atom == ATOM_NET_ACTIVE_WINDOW) {
+      xcb_window_t old_active = active_window;
+      active_window = get_active_window();
+
+      if (old_active != target_info.window_id) {
+        uint32_t mask[] = { XCB_EVENT_MASK_NO_EVENT };
+        xcb_change_window_attributes(x_conn, old_active, XCB_CW_EVENT_MASK, mask);
+      }
+      if (active_window != XCB_WINDOW_NONE && active_window != target_info.window_id) {
+        // listen for `_NET_WM_NAME`
+        uint32_t mask[] = { XCB_EVENT_MASK_PROPERTY_CHANGE };
+        xcb_change_window_attributes(x_conn, active_window, XCB_CW_EVENT_MASK, mask);
+      }
+      xcb_flush(x_conn);
+      check_and_handle_window(active_window, &target_info);
+    } else if (event->window == target_info.window_id && event->atom == ATOM_NET_WM_STATE) {
+      handle_fullscreen_xevent(&target_info);
+    } else if (event->window == active_window && event->atom == ATOM_NET_WM_NAME) {
+      check_and_handle_window(active_window, &target_info);
+    }
+    return;
+  }
+  printf("Event, %d\n", generic_event->response_type);
 }
 
 static void hook_thread(void* _arg) {
@@ -132,23 +257,36 @@ static void hook_thread(void* _arg) {
   atom_reply = xcb_intern_atom_reply(x_conn, xcb_intern_atom(x_conn, 0, strlen("UTF8_STRING"), "UTF8_STRING"), NULL);
   ATOM_UTF8_STRING = atom_reply->atom;
   free(atom_reply);
+  atom_reply = xcb_intern_atom_reply(x_conn, xcb_intern_atom(x_conn, 0, strlen("_NET_WM_STATE"), "_NET_WM_STATE"), NULL);
+  ATOM_NET_WM_STATE = atom_reply->atom;
+  free(atom_reply);
+  atom_reply = xcb_intern_atom_reply(x_conn, xcb_intern_atom(x_conn, 0, strlen("_NET_WM_STATE_FULLSCREEN"), "_NET_WM_STATE_FULLSCREEN"), NULL);
+  ATOM_NET_WM_STATE_FULLSCREEN = atom_reply->atom;
+  free(atom_reply);
 
   // listen for `_NET_ACTIVE_WINDOW` changes
   uint32_t mask[] = { XCB_EVENT_MASK_PROPERTY_CHANGE };
   xcb_change_window_attributes(x_conn, root, XCB_CW_EVENT_MASK, mask);
 
-  check_and_handle_window(get_active_window(), &target_info);
+  active_window = get_active_window();
+  if (active_window != XCB_WINDOW_NONE) {
+    // listen for `_NET_WM_NAME`
+    uint32_t mask[] = { XCB_EVENT_MASK_PROPERTY_CHANGE };
+    xcb_change_window_attributes(x_conn, active_window, XCB_CW_EVENT_MASK, mask);
+    xcb_flush(x_conn);
+    check_and_handle_window(active_window, &target_info);
+  }
 
   xcb_generic_event_t* event;
   while ((event = xcb_wait_for_event(x_conn))) {
+    event->response_type = event->response_type & ~0x80;
     hook_proc(event);
     free(event);
   }
 }
 
 void ow_start_hook(char* target_window_title, void* overlay_window_id) {
+  target_info.title = target_window_title;
+  overlay_info.window_id = *((xcb_window_t*)overlay_window_id);
   uv_thread_create(&hook_tid, hook_thread, NULL);
-  if (hook_tid == 0) {
-    //3
-  }
 }
